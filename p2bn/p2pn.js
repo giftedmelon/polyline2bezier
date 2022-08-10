d95ca97921d1b534423dfcf9ee89a6f0853391d3
giftedmelon <lyx1231996@gmail.com>
2022-08-10 16:47:40 +0900

    1

--- p2bn/p2pn.js
+++ p2bn/p2pn.js
@@ -30,7 +30,7 @@ function getInput() {
   }
   let arr = value.split(' ');
   let polyline = [];
-  for (let element of arr) {
+  for (let element of arr){
     let x = Number.parseFloat(element.split(',')[0]);
     let y = Number.parseFloat(element.split(',')[1]);
     let point = [x, y];
@@ -44,7 +44,7 @@ function getInput() {
 // document.getElementById("demo").innerHTML = 5 + 6;
 var polyline = [
   [72.0499,80.2825], [72.4009,80.4738], [73.3388,80.9843], [76.0439,82.4476], [77.5043,83.2223], [78.6601,83.8112] ,[79.2003,84.0646], [79.7521,84.2944] ,[81.8962,85.1245] ,[83.4281,85.7057], [84.416,86.0762]];
-var bezierCurves = polyline2bezier(polyline);
+var bezierCurves = polyline2bezier(polyline); 
 document.getElementById("demo").innerHTML = polyline;
 
 
---
+++ p2bn/test.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const cheerio = require('cheerio');
+var polyline2bezier= require('./polyline2bezier');
+
+
+function drawPath(segments) {
+  var d = [];
+  segments.forEach(function(segment, index) {
+    if (index === 0) {
+      d.push('M' + segment[0].x + ',' + segment[0].y + ' C');
+    }
+    segment.forEach(function(point, index_) {
+      if (index_ === 0) {
+        return;
+      }
+      d.push(point.x + ',' + point.y); 
+    });
+  });
+  if (segments.length > 0) {
+    d.push('z');
+  }
+  d = d.join(' ');
+  return ('d="' + d + '"');
+  //console.log('d="' + d + '"');
+}
+
+const path_to_svg = 'p2bn/c.png.svg';
+
+let $;
+let pathStringsArray= [];
+fs.readFile(path_to_svg,"utf8",(err,data) => {
+if (err) console.log(err);
+$ = cheerio.load(data, {xmlMode:true});
+
+const pathSet = $("polyline");
+pathSet.each(function(){ 
+  const points= $(this).attr("points");
+  let arrx= points.slice(0,-1);
+  // console.log(arrx);
+  let arr = arrx.split(' ');
+  let polyline = [];
+  for (let element of arr){
+    let x = Number.parseFloat(element.split(',')[0]);
+    let y = Number.parseFloat(element.split(',')[1]);
+    let point = [x, y];
+    polyline.push(point);
+  }
+  //console.log(polyline);
+  let bezierCurves = polyline2bezier(polyline);
+  let dd = drawPath(bezierCurves);
+  let d = dd.slice(0,-2);
+  pathStringsArray.push(d + '"');
+  console.log(pathStringsArray);
+  let svg = ""
+svg += '<?xml version="1.0" ?>\n'
+svg += '<svg xmlns="http://www.w3.org/2000/svg" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xlink="http://www.w3.org/1999/xlink" baseProfile="full" height="584px" version="1.1" viewBox="0 0 100 100" width="600px">\n'
+svg += '\t<defs/>\n'
+svg += '\t<g id="kvg:StrokePaths_00065" style="fill:none;stroke:#000000;stroke-width:1;stroke-linecap:round;stroke-linejoin:round;">\n'
+
+console.log("Len: " + pathStringsArray.length);
+
+for (let x = 0; x < pathStringsArray.length; x++) {
+  svg += '\t<path id="kvg:00065-s1"'
+  svg += pathStringsArray[x]
+  svg += '/>\n'
+}
+
+console.log("DUBUG:");
+console.log(pathStringsArray);
+
+svg += '</svg>'
+
+fs.writeFile('test.svg', svg, (err) =>{
+  if (err) throw err;
+  console.log('SVG written!');
+});
+});
+
+//console.log(pathStringsArray);
+});
+
+
+
+
+
